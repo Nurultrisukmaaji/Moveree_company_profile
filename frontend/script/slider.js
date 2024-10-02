d06f8425607f3cb5b34388d7a0d8ddab55b55b36
Nurultrisukmaaji <nurultrisukmaaji@gmail.com>
2024-10-02 18:43:20 +0700

    first commit

---
+++ frontend/script/slider.js
@@ -0,0 +1,105 @@
+// BUTTON CLOSE INFO METHOD
+$(document).ready(function () {
+    $("#btnProd1").click(function () {
+        $("#btnProd1").addClass('active-btn');
+        $("#btnProd2").removeClass('active-btn');
+        $("#btnProd3").removeClass('active-btn');
+        $("#btnProd4").removeClass('active-btn');
+        $("#btnProd5").removeClass('active-btn');
+        $("#btnProd6").removeClass('active-btn');
+        $("#btnProd7").removeClass('active-btn');
+        $("#btnProd8").removeClass('active-btn');
+        $("#btnProd9").removeClass('active-btn');
+
+    });
+    $("#btnProd2").click(function () {
+        $("#btnProd1").removeClass('active-btn');
+        $("#btnProd2").addClass('active-btn');
+        $("#btnProd3").removeClass('active-btn');
+        $("#btnProd4").removeClass('active-btn');
+        $("#btnProd5").removeClass('active-btn');
+        $("#btnProd6").removeClass('active-btn');
+        $("#btnProd7").removeClass('active-btn');
+        $("#btnProd8").removeClass('active-btn');
+        $("#btnProd9").removeClass('active-btn');
+
+    });
+    $("#btnProd3").click(function () {
+        $("#btnProd1").removeClass('active-btn');
+        $("#btnProd2").removeClass('active-btn');
+        $("#btnProd3").addClass('active-btn');
+        $("#btnProd4").removeClass('active-btn');
+        $("#btnProd5").removeClass('active-btn');
+        $("#btnProd6").removeClass('active-btn');
+        $("#btnProd7").removeClass('active-btn');
+        $("#btnProd8").removeClass('active-btn');
+        $("#btnProd9").removeClass('active-btn');
+    });
+    $("#btnProd4").click(function () {
+        $("#btnProd1").removeClass('active-btn');
+        $("#btnProd2").removeClass('active-btn');
+        $("#btnProd3").removeClass('active-btn');
+        $("#btnProd4").addClass('active-btn');
+        $("#btnProd5").removeClass('active-btn');
+        $("#btnProd6").removeClass('active-btn');
+        $("#btnProd7").removeClass('active-btn');
+        $("#btnProd8").removeClass('active-btn');
+        $("#btnProd9").removeClass('active-btn');
+    });
+    $("#btnProd5").click(function () {
+        $("#btnProd1").removeClass('active-btn');
+        $("#btnProd2").removeClass('active-btn');
+        $("#btnProd3").removeClass('active-btn');
+        $("#btnProd4").removeClass('active-btn');
+        $("#btnProd5").addClass('active-btn');
+        $("#btnProd6").removeClass('active-btn');
+        $("#btnProd7").removeClass('active-btn');
+        $("#btnProd8").removeClass('active-btn');
+        $("#btnProd9").removeClass('active-btn');
+    });
+    $("#btnProd6").click(function () {
+        $("#btnProd1").removeClass('active-btn');
+        $("#btnProd2").removeClass('active-btn');
+        $("#btnProd3").removeClass('active-btn');
+        $("#btnProd4").removeClass('active-btn');
+        $("#btnProd5").removeClass('active-btn');
+        $("#btnProd6").addClass('active-btn');
+        $("#btnProd7").removeClass('active-btn');
+        $("#btnProd8").removeClass('active-btn');
+        $("#btnProd9").removeClass('active-btn');
+    });
+    $("#btnProd7").click(function () {
+        $("#btnProd1").removeClass('active-btn');
+        $("#btnProd2").removeClass('active-btn');
+        $("#btnProd3").removeClass('active-btn');
+        $("#btnProd4").removeClass('active-btn');
+        $("#btnProd5").removeClass('active-btn');
+        $("#btnProd6").removeClass('active-btn');
+        $("#btnProd7").addClass('active-btn');
+        $("#btnProd8").removeClass('active-btn');
+        $("#btnProd9").removeClass('active-btn');
+    });
+    $("#btnProd8").click(function () {
+        $("#btnProd1").removeClass('active-btn');
+        $("#btnProd2").removeClass('active-btn');
+        $("#btnProd3").removeClass('active-btn');
+        $("#btnProd4").removeClass('active-btn');
+        $("#btnProd5").removeClass('active-btn');
+        $("#btnProd6").removeClass('active-btn');
+        $("#btnProd7").removeClass('active-btn');
+        $("#btnProd8").addClass('active-btn');
+        $("#btnProd9").removeClass('active-btn');
+    });
+    $("#btnProd9").click(function () {
+        $("#btnProd1").removeClass('active-btn');
+        $("#btnProd2").removeClass('active-btn');
+        $("#btnProd3").removeClass('active-btn');
+        $("#btnProd4").removeClass('active-btn');
+        $("#btnProd5").removeClass('active-btn');
+        $("#btnProd6").removeClass('active-btn');
+        $("#btnProd7").removeClass('active-btn');
+        $("#btnProd8").removeClass('active-btn');
+        $("#btnProd9").addClass('active-btn');
+    });
+
+});
